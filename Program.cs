7ca7b65c8af6e4ca24afbcf18265de318c3df266
iram96 <mariano.maselli@virgilio.it>
2022-05-12 17:57:52 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace csharp_es03 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci la base");
+            int rispA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("inserisci l'altezza");
+            int rispB = Convert.ToInt32(Console.ReadLine());
+
+            Rettangolo primoRett = new Rettangolo();
+            Rettangolo secondoRett = new Rettangolo();
+
+
+            Console.WriteLine(primoRett.infoRettangolo(rispA, rispB));
+            // Console.WriteLine("La tua Area è {0}", primoRett.calcolaArea(rispA, rispB));
+            // Console.WriteLine("Il tuo Perimetro è {0}", primoRett.calcolaPerimetro(rispA, rispB));
+        }
+    }
+}
---
+++ Rettangolo.cs
@@ -0,0 +1,38 @@
+﻿namespace csharp_es03
+{
+    internal class Rettangolo
+    {
+
+        //readonly rende la variabile immodificabile se non dal costruttore0
+        private int baseRettangolo;
+        private int altezzaRettangolo;
+        public Rettangolo()
+        {
+            baseRettangolo = 0;
+            altezzaRettangolo = 0;
+        }
+
+        public int calcolaArea(int baseRettangolo, int altezzaRettangolo)
+        {
+            return baseRettangolo * altezzaRettangolo;
+        }
+
+        public int calcolaPerimetro(int baseRettangolo, int altezzaRettangolo)
+        {
+            return (baseRettangolo + altezzaRettangolo) * 2;
+        }
+
+        public bool infoRettangolo(int baseRettangolo, int altezzaRettangolo)
+        {
+
+
+
+            Console.WriteLine("-- Dati Rettangolo --");
+            Console.WriteLine("-- base: {0} --", baseRettangolo);
+            Console.WriteLine("-- altezza: {0} --", altezzaRettangolo);
+            Console.WriteLine("-- area: {0} --", calcolaArea(baseRettangolo, altezzaRettangolo));
+            Console.WriteLine("-- perimetro: {0} --", calcolaPerimetro(baseRettangolo, altezzaRettangolo));
+            return true;
+        }
+    }
+}
